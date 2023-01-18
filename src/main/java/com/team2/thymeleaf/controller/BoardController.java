ced9ab134d9dfb37626c390779af0cd4a0596c9c
naegonggae <tkdtkd97@naver.com>
2023-01-18 09:23:09 +0900

    글 작성 구현

--- src/main/java/com/team2/thymeleaf/controller/BoardController.java
+++ src/main/java/com/team2/thymeleaf/controller/BoardController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,9 +21,20 @@ public class BoardController {
 
     @GetMapping("/list")
     public String list(Model model) {
-
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "/board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("board", new Board());
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String submit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
