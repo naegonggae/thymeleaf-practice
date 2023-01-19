ffa4ccf6b07839cf1641fda8c07722242ce56cc7
naegonggae <tkdtkd97@naver.com>
2023-01-19 11:28:51 +0900

    Page 기능 버튼구현까지

--- src/main/java/com/team2/thymeleaf/controller/BoardController.java
+++ src/main/java/com/team2/thymeleaf/controller/BoardController.java
@@ -5,6 +5,10 @@ import jakarta.validation.Valid;
 import com.team2.thymeleaf.model.Board;
 import com.team2.thymeleaf.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,8 +25,15 @@ public class BoardController {
     private final BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable) {
+        // http://localhost:8080/board/list?page=0
+        // http://localhost:8080/board/list?page=0&size=1
+        Page<Board> boards = boardRepository.findAll(pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        //boards.getTotalElements();
         model.addAttribute("boards", boards);
         return "/board/list";
     }
